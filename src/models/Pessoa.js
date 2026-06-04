b6c0f48011130ecf3773f6d1ca0c4df1742ff0d9
MarcleoSilva <marceloaraujosilva739@gmail.com>
2026-06-04 18:21:59 -0300

    Objeto pessoa

--- src/models/Pessoa.js
+++ src/models/Pessoa.js
@@ -1,7 +1,29 @@
-class Pessoa {
+export default class Pessoa {
     constructor(nome, telefone, email, endereco) {
         if (new.target === Pessoa) {
-            throw new Error('')
+            throw new Error('Não é possível instanciar uma pessoa inespecífica')
         }
+
+        this.nome = nome;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco
     }
+
+    getNome() { return this.nome};
+    setNome(nome) {this.nome = nome};
+
+    getTelefone() { return this.telefone};
+    setTelefone(Telefone) {this.telefone = Telefone};
+
+    getEmail() { return this.email};
+    setEmail(Email) {this.email = Email};
+
+    getEndereco() { return this.endereco};
+    setEndereco(Endereco) {this.endereco = Endereco};
+
+    toString() {
+        return `nome = '${this.nome}', telefone = '${this.telefone}', email = '${this.email}', endereco = '${this.endereco}'`
+    }
+    
 }
